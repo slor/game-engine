a6650afbb580dff47eec8b494763c14e085a45ec
Elliott Hoffman <elliott.hoffman@cerner.com>
2019-06-18 13:01:09 -0500

    Added matrix and global namespace.
    The global namespace GE is for encapsulation and a simple way to run tests. The matrix class will the vector class I think... some reuse of scaling and adding for when it comes to multiplying matricies.

---
+++ matrix.js
@@ -0,0 +1,8 @@
+;(() => {
+    class Matrix{}
+
+    function test () {}
+
+    $GE.Matrix = Matrix;
+    $GE.tests.push(test);
+})();
--- vector.js
+++ vector.js
@@ -1,56 +1,68 @@
-class Vector{
-    constructor(components){
-        this._components = components;
-    }
-
-    get components(){
-        return this._components;
-    }
-
-    get dimension(){
-        return this.components.length;
-    }
-
-    scale(scalar){
-        return new Vector(this.components.map(component => scalar * component ));
-    }
-
-    add(vector){
-        let sum = [];
-
-        this.components.forEach((element, index) => {
-            sum[index] = element + vector.components[index];
-        });
-        return new Vector(sum);
-    }
-
-    equals(vector){
-        // true if elements are identical
-        if(this.dimension !== vector.dimension){
-            return false;
+;(() => {
+    class Vector{
+        constructor(components){
+            this._components = components;
         }
 
-        let result = true;
-        this.components.reduce((elem, index) => elem !== vector.components[index], result);
+        get components(){
+            return this._components;
+        }
 
-        return result;
+        get dimension(){
+            return this.components.length;
+        }
+
+        scale(scalar){
+            return new Vector(this.components.map(component => scalar * component ));
+        }
+
+        add(vector){
+            let sum = [];
+
+            this.components.forEach((element, index) => {
+                sum[index] = element + vector.components[index];
+            });
+            return new Vector(sum);
+        }
+
+        equals(vector){
+            // true if elements are identical
+            if(this.dimension !== vector.dimension){
+                return false;
+            }
+
+            let result = true;
+            this.components.forEach(function (elem, index){
+                if(elem !== vector.components[index]){
+                    result = false;
+                }
+            });
+
+            return result;
+        }
     }
-}
 
-// TODO: write proper tests
-let vec = new Vector([1, 2, 3]);
+    function test () {
+        let vec = new Vector([1, 2, 3]);
 
-// Vector.dimension
-console.assert(3 === vec.dimension);
+        // Vector.dimension
+        $GE.assert(3 === vec.dimension);
 
-// Vector.equals()
-console.assert(false === vec.equals(new Vector([])));
-console.assert(false === vec.equals(new Vector([1, 2])));
-console.assert(true === vec.equals(new Vector([1, 2, 3])));
-console.assert(false === vec.equals(new Vector([1, 2, 3, 4])));
+        // Vector.equals()
+        $GE.assertFalse(vec.equals(new Vector([])));
+        $GE.assertFalse(vec.equals(new Vector([1, 2])));
+        $GE.assert(vec.equals(new Vector([1, 2, 3])));
+        $GE.assertFalse(vec.equals(new Vector([1, 2, 3, 4])));
 
-// Vector.scale()
-console.assert(vec.scale(2).equals(new Vector([2, 4, 6])));
+        // Vector.scale()
+        $GE.assertFalse(vec.scale(2).equals(new Vector([2, 4, 7])));
+        $GE.assert(vec.scale(2).equals(new Vector([2, 4, 6])));
 
-// Vector.add()
-console.assert(vec.add(new Vector([1, 1, 1])).equals(new Vector([2, 3, 4])));
+        // Vector.add()
+        $GE.assertFalse(vec.add(new Vector([2, 1, 1])).equals(new Vector([2, 3, 4])));
+        $GE.assert(vec.add(new Vector([1, 1, 1])).equals(new Vector([2, 3, 4])));
+    }
+
+    $GE.Vector = Vector;
+    $GE.tests.push(test);
+})();

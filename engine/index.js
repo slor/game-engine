2689dc76c4c67140594971e4a2029dc72fecced1
Elliott Hoffman <elliott.hoffman@gmail.com>
2019-06-22 20:43:41 -0500

    Moved engine into Game class

--- engine/index.js
+++ engine/index.js
@@ -1,92 +1,76 @@
-// A toy game engine.
+class Game {
+    constructor(window) {
+        this.SECOND = 1000.0;
+        this.TICK = 1000.0 / 60.0; // In ms
+        this.DEBUG = true;
 
-Game = {
-    SECOND: 1000.0,
-    TICK: 1000.0 / 60.0, // In ms
-    DEBUG: true,
-    keysPressed: {},
-    mousePosX: 0,
-    mousePosY: 0,
-    mousePressed: false,
-    document: document,
-    canvas: document.getElementById("screen"),
-    context: null,
-    lastSec: 0, // Timestamp in ms when we last did 1000ms of frames
-    lastTick: 0, // Timestamp in ms when we last did one TICK
-    lastFps: 0, // Frames/seconds
-    framesLastSec: 0, // Frames since last second
-};
+        this.keysPressed = {};
+        this.mousePosX = 0;
+        this.mousePosY = 0;
+        this.mousePressed = false;
+        
+        this.lastSec = 0; // Timestamp in ms when we last did 1000ms of frames
+        this.lastTick = 0; // Timestamp in ms when we last did one TICK
+        this.lastFps = 0; // Frames/seconds
+        this.framesLastSec = 0; // Frames since last secthis
 
-Game.document.addEventListener('keydown', e => {
-    e = e || window.event;
-    Game.keysPressed[e.key] = true;
-    e.preventDefault();
-});
+        this.window = window;
+        this.document = this.window.document;
+        this.canvas = this.document.getElementById("screen");
+        this.context = this.canvas.getContext('2d');
+    }
 
-Game.document.addEventListener('keyup', e => {
-    e = e || window.event;
-    delete Game.keysPressed[e.key];
-    e.preventDefault();
-});
+    keyDown(e){
+        this.keysPressed[e.key] = true;
+    }
 
-Game.canvas.addEventListener('mousemove', e => {
-    e = e || window.event;
+    keyUp(e){
+        delete this.keysPressed[e.key];
+    }    
 
-    let rect = Game.canvas.getBoundingClientRect();
+    mouseMove(e) {
+        const rect = this.canvas.getBoundingClientRect();
+        this.mousePosX = e.clientX - rect.left;
+        this.mousePosY = e.clientY - rect.top;
+    }
 
-    Game.mousePosX = e.clientX - rect.left;
-    Game.mousePosY = e.clientY - rect.top;
-    e.preventDefault();
-});
+    mouseDown(e) {
+        this.mousePressed = true;
+    }
 
-Game.canvas.addEventListener('mousedown', e => {
-    e = e || window.event;
-    Game.mousePressed = true;
-});
+    mouseUp(e) {
+        this.mousePressed = false;
+    }
+    
+    frame(now){
+        const timeSinceLastTick = now - this.lastTick;
 
-Game.canvas.addEventListener('mouseup', e => {
-    e = e || window.event;
-    Game.mousePressed = false;
-});
-
-Game.context = Game.canvas.getContext("2d");
-
-;(function () {
-    function main(now){
-        window.requestAnimationFrame(main)
-
-        const timeSinceLastTick = now - Game.lastTick;
-
-        if (timeSinceLastTick >= Game.TICK) {
-            Game.lastTick++;
-            Game.framesLastSec++;
-            update();
-            draw();
+        if (timeSinceLastTick >= this.TICK) {
+            this.lastTick++;
+            this.framesLastSec++;
+            this.update();
+            this.draw();
         }
 
         const deltaSecs = now / 1000.0;
-        if (deltaSecs >= Game.lastSec + 1 ) {
-            Game.lastSec = deltaSecs;
-            Game.lastFps = Game.framesLastSec;
-            Game.framesLastSec = 0;
+        if (deltaSecs >= this.lastSec + 1 ) {
+            this.lastSec = deltaSecs;
+            this.lastFps = this.framesLastSec;
+            this.framesLastSec = 0;
         }
     }
 
-    function update(){
-        if(Game.DEBUG){
-            updateDebugInfo();
-        }
+    update(){
+        // TODO
     }
 
-    function updateDebugInfo(){}
-
-    function clear() {
-        Game.context.clearRect(0, 0,Game.canvas.width,Game.canvas.height);
+    clear(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
-    function drawDebugInfo(){
-        const ctx = Game.context;
-        const can = Game.canvas;
+    drawDebugInfo(){
+        const ctx = this.context;
+        const can = this.canvas;
 
         // Frame and timing info
         const DEBUG_SIZE = 16; //px
@@ -94,27 +78,25 @@ Game.context = Game.canvas.getContext("2d");
         ctx.font = `${DEBUG_SIZE}px sans-serif`;
         ctx.textAlign = 'right';
 
-        ctx.fillText(`${Game.lastTick}`.split('.')[0], can.width, DEBUG_SIZE);
-        ctx.fillText(`${Game.lastSec}`.split('.')[0], can.width, DEBUG_SIZE * 2);
-        ctx.fillText(`${Game.lastFps}`.slice(0, 5), can.width, DEBUG_SIZE * 3);
+        ctx.fillText(`${this.lastTick}`.split('.')[0], can.width, DEBUG_SIZE);
+        ctx.fillText(`${this.lastSec}`.split('.')[0], can.width, DEBUG_SIZE * 2);
+        ctx.fillText(`${this.lastFps}`.slice(0, 5), can.width, DEBUG_SIZE * 3);
 
         // Mouse
-        ctx.fillText(`${Game.mousePosX}, ${Game.mousePosY}`, can.width, DEBUG_SIZE * 4);
-        if (Game.mousePressed) {
+        ctx.fillText(`${this.mousePosX}, ${this.mousePosY}`, can.width, DEBUG_SIZE * 4);
+        if (this.mousePressed) {
             ctx.fillText('Click!', can.width, DEBUG_SIZE * 5);
         }
 
         // Keyboard
-        ctx.fillText(`${Object.keys(Game.keysPressed)}`, can.width, DEBUG_SIZE * 6);
+        ctx.fillText(`${Object.keys(this.keysPressed)}`, can.width, DEBUG_SIZE * 6);
     }
 
-    function draw() {
-        clear();
+    draw() {
+        this.clear();
 
-        if(Game.DEBUG){
-            drawDebugInfo();
+        if(this.DEBUG){
+            this.drawDebugInfo();
         }
     }
-
-    window.requestAnimationFrame(main)
-})();
+}
---
+++ sprite/index.js
@@ -0,0 +1,28 @@
+class Loader {
+	// Load a sprite sheet once.
+	constructor(src, loadedCallback){
+		this.sheet = new Image();
+		this.sheet.addEventListener('load', loadedCallback, false);
+		this.sheet.src = src;
+	}
+}
+
+
+class Sprite {
+	// slice a sprite from a sheet
+	constructor(sheet, ctx, sx, sy, sw, sh){
+		this.sheet = sheet;
+		this.sx = sx;
+		this.sy = sy;
+		this.sw = sw;
+		this.sh = sh;
+		this.x = 0;
+		this.y = 0;
+		this.ctx = ctx;
+	}
+
+	draw(){
+		this.ctx.drawImage(this.sheet, this.sx, this.sy, this.sw, this.sh, 
+					  this.x, this.y, this.sw, this.sh);
+	}
+}
